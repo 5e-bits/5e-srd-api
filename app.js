afec0951607e8c127cf9291111727290c4619b45
Adrian Padua <adrpadua@gmail.com>
2016-12-15 21:58:03 -0800

    added view engine to app.js

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
