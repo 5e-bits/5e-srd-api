7a2df368b9f9382e72b35af4016327063317ad7f
adrpadua <adrpadua@gmail.com>
2016-12-20 10:48:22 -0800

    Added query search by character class

--- server.js
+++ server.js
@@ -41,22 +41,34 @@ router.route('/spells')
 .get((req,res) => {
 
   let query_name = req.query.name;
+  let query_class = req.query.class;
 
-  if (query_name === undefined) {
-    Spell.find((err,spells) => {
-      if (err) {
-        res.send(err);
-      }
+  if (query_name !== undefined) {
 
-      res.status(200).json(spells);
-    })
-  } else {
     Spell.findOne({ name: query_name }, (err,spell) => {
       if (err) {
         res.send(err);
       }
 
       res.status(200).json(spell);
+    })
+  } else if (query_class !== undefined) {
+    
+    Spell.find({ class: {name: query_class}}, (err,spells) => {
+      if (err) {
+        res.send(err);
+      }
+
+      res.status(200).json(spells);
+    })
+
+  } else {
+    Spell.find((err,spells) => {
+      if (err) {
+        res.send(err);
+      }
+
+      res.status(200).json(spells);
     })
   }
 
