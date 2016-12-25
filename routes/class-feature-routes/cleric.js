555cece97e53c78b40adc5fed57a86ae150c79b4
adrpadua <adrpadua@gmail.com>
2016-12-25 14:29:50 -0800

    Added level routes for features

--- routes/class-feature-routes/cleric.js
+++ routes/class-feature-routes/cleric.js
@@ -21,4 +21,19 @@ router.route('/')
 
 })
 
+router.route('/level/:level')
+.get((req,res) => {
+  ClassFeature.find({class: class_name, level: parseInt(req.params.level) }, (err, features) => {
+    if (err) {
+      res.send(err);
+    }
+  }).sort({index: 'asc'}).exec((err, features) => {
+    if (err) {
+      res.send(err);
+    }
+    res.status(200).json(features);
+  })
+
+})
+
 module.exports = router;
