5b1722e88848ea3ae2898e5b90edfeaa9f4d900b
adrpadua <adrpadua@gmail.com>
2016-12-31 03:14:08 -0800

    Added some more documentation and fixed startingequipment routes

--- models/startingequipment.js
+++ models/startingequipment.js
@@ -2,6 +2,11 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var StartingEquipmentSchema   = new Schema({
+    class: {
+        name: String,
+        url: String
+    },
+    url: String
 });
 
 module.exports = mongoose.model('StartingEquipment', StartingEquipmentSchema, 'startingequipment');
