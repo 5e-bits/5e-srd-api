0449fae787fbbe1128143b638499b7608fe13401
Adrian Padua <adrpadua@gmail.com>
2017-01-16 18:29:56 -0800

    added * to access control

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(morgan('short'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
