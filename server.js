4739a9806aa1d377820ed234d2d74be1d1373a98
adrpadua <adrpadua@gmail.com>
2017-03-13 12:33:36 -0700

    set application/json as main content type

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(morgan('short'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Content-Type", "application/json")
     next();
 });
 
