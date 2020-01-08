ffaad56c1aedf359469bdc22cd6eab61eee7623b
Christopher Ward <cdurianward@gmail.com>
2020-01-08 15:07:18 -0800

    That's useless for now.

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,6 +1,3 @@
-// const request = require('supertest');
-// const app = require('../server');
-
 describe('Sample Test', () => {
   it('should test that true === true', () => {
     expect(true).toBe(true);
