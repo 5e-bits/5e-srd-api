deaa0636edac918c7c2281a23c6d97f75df90828
Christopher Ward <cdurianward@gmail.com>
2020-01-10 15:47:13 -0800

    Missed one.

--- controllers/api/weaponPropertyController.js
+++ controllers/api/weaponPropertyController.js
@@ -1,36 +1,32 @@
 const WeaponProperty = require('../../models/weaponProperty');
 const utility = require('./utility');
 
-exports.index = (req, res, next) => {
+exports.index = async (req, res, next) => {
   const search_queries = {};
   if (req.query.name !== undefined) {
     search_queries.name = req.query.name;
   }
 
-  WeaponProperty.find(search_queries, (err, _data) => {
-    if (err) {
-      next(err);
-    }
-  })
+  await WeaponProperty.find(search_queries)
     .sort({ index: 'asc' })
-    .exec((err, data) => {
-      if (err) {
-        next(err);
-      }
+    .then(data => {
       res.status(200).json(utility.NamedAPIResource(data));
+    })
+    .catch(err => {
+      next(err);
     });
 };
 
-exports.show = (req, res, next) => {
-  WeaponProperty.findOne({ index: req.params.index }, (err, data) => {
-    if (err) {
+exports.show = async (req, res, next) => {
+  await WeaponProperty.findOne({ index: req.params.index })
+    .then(data => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({ error: 'Not found' });
+      }
+    })
+    .catch(err => {
       next(err);
-    }
-
-    if (data) {
-      res.status(200).json(data);
-    } else {
-      res.status(404).json({ error: 'Not found' });
-    }
-  });
+    });
 };
