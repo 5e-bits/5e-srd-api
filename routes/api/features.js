b270dd2e905c00307fa74f1afaf3bb12d628f607
Christopher Ward <cdurianward@gmail.com>
2020-01-11 02:01:06 -0800

    Well. That route doesn't exist anymore.

--- routes/api/features.js
+++ routes/api/features.js
@@ -4,7 +4,4 @@ const FeatureController = require('../../controllers/api/featureController');
 router.get('/', FeatureController.index);
 router.get('/:index', FeatureController.show);
 
-// TODO: This doesn't make any sense here and should move to class
-router.get('/:index/level/:level', FeatureController.showForLevel);
-
 module.exports = router;
