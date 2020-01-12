7ea6a7ec540fb074a849d9f9b65e099ac5cdcb58
Christopher Ward <cdurianward@gmail.com>
2020-01-11 16:33:05 -0800

    Some basic integration  tests.

--- tests/integration/server.itest.js
+++ tests/integration/server.itest.js
@@ -17,6 +17,7 @@ describe('/', () => {
     expect(res.statusCode).toEqual(200);
   });
 });
+
 describe('/docs', () => {
   it('should load the page', async () => {
     const res = await request(app).get('/docs');
@@ -29,5 +30,485 @@ describe('/api', () => {
     const res = await request(app).get('/api');
     expect(res.statusCode).toEqual(200);
     expect(res.body).toHaveProperty('ability-scores');
+  });
+});
+
+describe('/api/ability-scores', () => {
+  it('should list ability scores', async () => {
+    const res = await request(app).get('/api/ability-scores');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/ability-scores/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/ability-scores');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/ability-scores/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/ability-scores/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/classes', () => {
+  it('should list classes', async () => {
+    const res = await request(app).get('/api/classes');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/classes/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/classes');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/classes/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/classes/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/conditions', () => {
+  it('should list conditions', async () => {
+    const res = await request(app).get('/api/conditions');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/conditions/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/conditions');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/conditions/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/conditions/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/damage-types', () => {
+  it('should list damage types', async () => {
+    const res = await request(app).get('/api/damage-types');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/damage-types/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/damage-types');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/damage-types/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/damage-types/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/equipment-categories', () => {
+  it('should list equipment categories', async () => {
+    const res = await request(app).get('/api/equipment-categories');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/equipment-categories/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/equipment-categories');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/equipment-categories/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/equipment-categories/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/equipment', () => {
+  it('should list equipment', async () => {
+    const res = await request(app).get('/api/equipment');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/equipment/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/equipment');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/equipment/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/equipment/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/features', () => {
+  it('should list features', async () => {
+    const res = await request(app).get('/api/features');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/features/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/features');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/features/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/features/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/languages', () => {
+  it('should list languages', async () => {
+    const res = await request(app).get('/api/languages');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/languages/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/languages');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/languages/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/languages/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/magic-schools', () => {
+  it('should list magic schools', async () => {
+    const res = await request(app).get('/api/magic-schools');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/magic-schools/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/magic-schools');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/magic-schools/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/magic-schools/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/monsters', () => {
+  it('should list monsters', async () => {
+    const res = await request(app).get('/api/monsters');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/monsters/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/monsters');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/monsters/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/monsters/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/proficiencies', () => {
+  it('should list proficiencies', async () => {
+    const res = await request(app).get('/api/proficiencies');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/proficiencies/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/proficiencies');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/proficiencies/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/proficiencies/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/races', () => {
+  it('should list races', async () => {
+    const res = await request(app).get('/api/races');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/races/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/races');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/races/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/races/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/skills', () => {
+  it('should list skills', async () => {
+    const res = await request(app).get('/api/skills');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/skills/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/skills');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/skills/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/skills/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/spellcasting', () => {
+  it('should list spellcasting', async () => {
+    const res = await request(app).get('/api/spellcasting');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/spellcasting/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/spellcasting');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/spellcasting/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 30;
+      const showRes = await request(app).get(`/api/spellcasting/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/spells', () => {
+  it('should list spells', async () => {
+    const res = await request(app).get('/api/spells');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/spells/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/spells');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/spells/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/spells/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/starting-equipment', () => {
+  it('should list starting equipment', async () => {
+    const res = await request(app).get('/api/starting-equipment');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/starting-equipment/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/starting-equipment');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/starting-equipment/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 30;
+      const showRes = await request(app).get(`/api/starting-equipment/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/subclasses', () => {
+  it('should list subclasses', async () => {
+    const res = await request(app).get('/api/subclasses');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/subclasses/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/subclasses');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/subclasses/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/subclasses/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/subraces', () => {
+  it('should list subraces', async () => {
+    const res = await request(app).get('/api/subraces');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/subraces/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/subraces');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/subraces/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/subraces/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/traits', () => {
+  it('should list traits', async () => {
+    const res = await request(app).get('/api/traits');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/traits/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/traits');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/traits/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/traits/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
+  });
+});
+
+describe('/api/weapon-properties', () => {
+  it('should list weapon properties', async () => {
+    const res = await request(app).get('/api/weapon-properties');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+});
+describe('/api/weapon-properties/:index', () => {
+  it('should return one object', async () => {
+    const indexRes = await request(app).get('/api/weapon-properties');
+    const index = indexRes.body.results[0].index;
+    const showRes = await request(app).get(`/api/weapon-properties/${index}`);
+    expect(showRes.statusCode).toEqual(200);
+    expect(showRes.body.index).toEqual(index);
+  });
+  describe('with an invalid index', () => {
+    it('should return one object', async () => {
+      const invalidIndex = 'invalid-index';
+      const showRes = await request(app).get(`/api/weapon-properties/${invalidIndex}`);
+      expect(showRes.statusCode).toEqual(404);
+    });
   });
 });
