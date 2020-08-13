b12555bc7aef2b9627ac25b4b18222c2e364fab5
Chris Ward <cdurianward@gmail.com>
2020-08-13 12:38:46 -0700

    Starting Equipment cutover to string indices (#78)
    * Add yarn lock file
    
    * Switch to string index.
    
    * I think that's everything for switching this over.
    
    * fix one test
    
    * There we go!
    
    * Oh. We use NPM.
    
    * Empty commit

--- src/models/startingEquipment.js
+++ src/models/startingEquipment.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var StartingEquipmentSchema = new Schema({
-  index: Number,
+  index: String,
   class: {
     name: String,
     url: String
