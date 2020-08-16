dd8045e28d5096f73d7c47d255bfe06c9f2992cb
Fergus Bentley <fergus.bentley@gmail.com>
2020-08-16 12:34:50 +0100

    Oops! Was in JSON mode!

--- src/tests/controllers/api/subclassController.test.js
+++ src/tests/controllers/api/subclassController.test.js
@@ -101,17 +101,17 @@ describe('show', () => {
 describe('showLevelsForSubclass', () => {
   const findDoc = [
     {
-      index: "barbarian-1",
+      index: 'barbarian-1',
       level: 1,
       url: '/api/subclasses/barbarian/level/1'
     },
     {
-      index: "barbarian-2",
+      index: 'barbarian-2',
       level: 2,
       url: '/api/subclasses/barbarian/level/2'
     },
     {
-      index: "barbarian-3",
+      index: 'barbarian-3',
       level: 3,
       url: '/api/subclasses/barbarian/level/3'
     }
@@ -154,7 +154,7 @@ describe('showLevelsForSubclass', () => {
 
 describe('showLevelForSubclass', () => {
   const findOneDoc = {
-    index: "barbarian-1",
+    index: 'barbarian-1',
     level: 1,
     url: '/api/classes/barbarian/level/1'
   };
