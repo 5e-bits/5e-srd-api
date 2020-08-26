d702b75cec4a26698507e2cf6b22acc82369b100
Christopher Ward <cdurianward@gmail.com>
2020-08-26 16:08:25 -0700

    Drop _id from select

--- src/models/class.js
+++ src/models/class.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ClassSchema = new Schema({
+  _id: {
+    type: String,
+    select: false
+  },
   index: String,
   name: String,
   url: String
--- src/models/skill.js
+++ src/models/skill.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var SkillSchema = new Schema({
+  _id: {
+    type: String,
+    select: false
+  },
   index: String,
   name: String,
   url: String
