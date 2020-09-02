45af98d2cde8dac0b38f507940805a8a36a91dad
Christopher Ward <cdurianward@gmail.com>
2020-09-02 14:16:38 -0700

    Flush redis on deploy

--- src/start.js
+++ src/start.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { mongodbUri } = require('./util');
+const { mongodbUri, redisClient } = require('./util');
 const app = require('./server');
 
 // Connect to database and start the serverfuser
@@ -7,7 +7,12 @@ mongoose
   .connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(_database => {
     console.log('Database connection ready');
-
+  })
+  .then(() => {
+    console.log('Flushing Redis');
+    return redisClient.flushall();
+  })
+  .then(() => {
     const server = app.listen(process.env.PORT || 3000, () => {
       const port = server.address().port;
       console.log(`Listening on port ${port}!`);
