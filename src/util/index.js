76eff1cee451ad3e7bc070e30d24ebf0db995efb
youknowme786 <aliarfeen911@gmail.com>
2020-09-02 20:41:47 -0500

    Break out environment variables

--- src/util/index.js
+++ src/util/index.js
@@ -1,14 +1,14 @@
 const redis = require('redis');
+const { redisUrl, bugsnagApiKey, mongodbUri } = require('./environmentVariables');
 
-const redisUrl = process.env.REDIS_URL || '';
 const redisClient = redis.createClient(redisUrl);
 const closeRedisClient = () => {
   redisClient.quit();
 };
 
 module.exports = {
-  bugsnagApiKey: process.env.BUGSNAG_API_KEY || 'local',
-  mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost/5e-database',
+  bugsnagApiKey,
+  mongodbUri,
   redisClient,
   closeRedisClient
 };
