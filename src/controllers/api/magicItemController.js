42c61bc475461ec8345a127901aeb500e4d135d4
Chris Ward <cdurianward@gmail.com>
2020-10-01 13:24:41 -0700

    Add controller

---
+++ src/controllers/api/magicItemController.js
@@ -0,0 +1,32 @@
+const MagicItem = require('../../models/magicItem');
+const utility = require('./utility');
+
+exports.index = async (req, res, next) => {
+  const search_queries = {};
+  if (req.query.name !== undefined) {
+    search_queries.name = { $regex: new RegExp(utility.escapeRegExp(req.query.name), 'i') };
+  }
+
+  await MagicItem.find(search_queries)
+    .sort({ index: 'asc' })
+    .then(data => {
+      res.status(200).json(utility.NamedAPIResource(data));
+    })
+    .catch(err => {
+      next(err);
+    });
+};
+
+exports.show = async (req, res, next) => {
+  await MagicItem.findOne({ index: req.params.index })
+    .then(data => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({ error: 'Not found' });
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+};
