088cfc7d4a2c5d26ba33d183e6f7e995d4ea8cf9
Chris Ward <cdurianward@gmail.com>
2020-10-01 13:22:32 -0700

    Add in tests.

--- src/tests/integration/server.itest.js
+++ src/tests/integration/server.itest.js
@@ -452,8 +452,50 @@ describe('/api/languages', () => {
   });
 });
 
+describe('/api/magic-items', () => {
+  it('should list magic items', async () => {
+    const res = await request(app).get('/api/magic-items');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.results.length).not.toEqual(0);
+  });
+  describe('with name query', () => {
+    it('returns the named object', async () => {
+      const indexRes = await request(app).get('/api/magic-items');
+      const name = indexRes.body.results[1].name;
+      const res = await request(app).get(`/api/magic-items?name=${name}`);
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.results[0].name).toEqual(name);
+    });
+
+    it('is case insensitive', async () => {
+      const indexRes = await request(app).get('/api/magic-items');
+      const name = indexRes.body.results[1].name;
+      const queryName = name.toLowerCase();
+      const res = await request(app).get(`/api/magic-items?name=${queryName}`);
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.results[0].name).toEqual(name);
+    });
+  });
+  describe('/api/magic-items/:index', () => {
+    it('should return one object', async () => {
+      const indexRes = await request(app).get('/api/magic-items');
+      const index = indexRes.body.results[0].index;
+      const showRes = await request(app).get(`/api/magic-items/${index}`);
+      expect(showRes.statusCode).toEqual(200);
+      expect(showRes.body.index).toEqual(index);
+    });
+    describe('with an invalid index', () => {
+      it('should return one object', async () => {
+        const invalidIndex = 'invalid-index';
+        const showRes = await request(app).get(`/api/magic-items/${invalidIndex}`);
+        expect(showRes.statusCode).toEqual(404);
+      });
+    });
+  });
+});
+
 describe('/api/magic-schools', () => {
-  it('should list magic schools', async () => {
+  it('should list magic items', async () => {
     const res = await request(app).get('/api/magic-schools');
     expect(res.statusCode).toEqual(200);
     expect(res.body.results.length).not.toEqual(0);
