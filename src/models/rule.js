0b767055fb1a7173afbb993ecac76572d1e1faa7
Zach Johnson <zjohnson@adobe.com>
2020-10-01 21:11:26 -0600

    fixed formatting

--- src/models/rule.js
+++ src/models/rule.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var RuleSchema = new Schema({
-	_id: {
-		type: String,
-		select: false
-	},
-	index: String,
-	name: String,
-	url: String
+  _id: {
+    type: String,
+    select: false
+  },
+  index: String,
+  name: String,
+  url: String
 });
 
 module.exports = mongoose.model('Rule', RuleSchema, 'rules');
