a7481b3cddb9029aa566a98bf42796bc2743d2ac
Chris Ward <cdurianward@gmail.com>
2020-10-02 14:24:49 -0700

    Just a little sorting out.

--- src/util/index.js
+++ src/util/index.js
@@ -1,14 +1,14 @@
 const redis = require('redis');
+const { bugsnagApiKey, mongodbUri, redisUrl } = require('./environmentVariables');
 
-const redisUrl = process.env.REDIS_URL || '';
 const redisClient = redis.createClient(redisUrl);
 const closeRedisClient = () => {
   redisClient.quit();
 };
 
 module.exports = {
-  bugsnagApiKey: process.env.BUGSNAG_API_KEY || 'local',
-  mongodbUri: process.env.MONGODB_URI || 'mongodb://localhost/5e-database',
+  bugsnagApiKey,
+  mongodbUri,
   redisClient,
   closeRedisClient
 };
