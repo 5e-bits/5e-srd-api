5c3e4767780554879bf811652605a4b505345fbb
Christopher Ward <cdurianward@gmail.com>
2020-10-05 15:15:55 -0700

    That's three

--- src/routes/api/subraces.js
+++ src/routes/api/subraces.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const SubraceController = require('../../controllers/api/subraceController');
 
-router.get('/', SubraceController.index);
-router.get('/:index', SubraceController.show);
+router.get('/', function(req, res, next) {
+  SubraceController.index(req, res, next);
+});
+
+router.get('/:index', function(req, res, next) {
+  SubraceController.show(req, res, next);
+});
 
 router.get('/:index/traits', SubraceController.showTraitsForSubrace);
 router.get('/:index/proficiencies', SubraceController.showProficienciesForSubrace);
--- src/routes/api/weaponProperties.js
+++ src/routes/api/weaponProperties.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const WeaponPropertyController = require('../../controllers/api/weaponPropertyController');
 
-router.get('/', WeaponPropertyController.index);
-router.get('/:index', WeaponPropertyController.show);
+router.get('/', function(req, res, next) {
+  WeaponPropertyController.index(req, res, next);
+});
+
+router.get('/:index', function(req, res, next) {
+  WeaponPropertyController.show(req, res, next);
+});
 
 module.exports = router;
