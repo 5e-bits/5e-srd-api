3eb448ff964a2dfeba22b35e3fa6eb55d22eadf4
Christopher Ward <cdurianward@gmail.com>
2020-11-08 14:46:07 -0800

    Forgot to expose the util

--- src/util/index.js
+++ src/util/index.js
@@ -1,8 +1,10 @@
 const redisClient = require('./RedisClient');
+const apolloClient = require('./ApolloClient');
 const { bugsnagApiKey, mongodbUri } = require('./environmentVariables');
 
 module.exports = {
   bugsnagApiKey,
   mongodbUri,
-  redisClient
+  redisClient,
+  apolloClient
 };
