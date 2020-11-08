bb107ff6994dbeb0debfd544a8f94771eedbb0f4
Christopher Ward <cdurianward@gmail.com>
2020-11-08 14:32:38 -0800

    Setup the client

---
+++ src/util/ApolloClient.js
@@ -0,0 +1,35 @@
+const Realm = require('realm-web');
+const { ApolloClient, HttpLink, InMemoryCache } = require('@apollo/client');
+const { realmAppId, realmApiKey } = require('./environmentVariables');
+
+// Connect to your MongoDB Realm app
+const app = new Realm.App(realmAppId);
+
+// Get a valid Realm user access token to authenticate requests
+async function getValidAccessToken() {
+  if (!app.currentUser) {
+    // If no user is logged in, log in
+    const credentials = Realm.Credentials.apiKey(realmApiKey);
+    await app.logIn(credentials);
+  } else {
+    // The logged in user's access token might be stale,
+    // Refreshing custom data also refreshes the access token
+    await app.currentUser.refreshCustomData();
+  }
+  // Get a valid access token for the current user
+  return app.currentUser.accessToken;
+}
+
+const graphql_url = `https://realm.mongodb.com/api/client/v2.0/app/${realmAppId}/graphql`;
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: graphql_url }),
+  cache: new InMemoryCache(),
+  fetch: async (uri, options) => {
+    const accessToken = await getValidAccessToken();
+    options.headers.Authorization = `Bearer ${accessToken}`;
+    return fetch(uri, options);
+  }
+});
+
+module.exports = client;
--- src/util/environmentVariables.js
+++ src/util/environmentVariables.js
@@ -1,9 +1,13 @@
 const redisUrl = process.env.REDIS_URL || '';
 const bugsnagApiKey = process.env.BUGSNAG_API_KEY || 'local';
 const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost/5e-database';
+const realmAppId = process.env.REALM_APP_ID || '';
+const realmApiKey = process.env.REALM_API_KEY || '';
 
 module.exports = {
   redisUrl,
   bugsnagApiKey,
-  mongodbUri
+  mongodbUri,
+  realmAppId,
+  realmApiKey
 };
