445ad48957073ad5c966ad2647f4b7bbb9ca4008
Christopher Ward <cdurianward@gmail.com>
2020-11-10 14:41:58 -0800

    Moving file to middleware folder

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { bugsnagMiddleware } = require('./middleware/bugsnag');
-const { createApolloMiddleware, apolloAvailable } = require('./apollo/server');
+const { createApolloMiddleware, apolloAvailable } = require('./middleware/apolloServer');
 
 const createApp = async () => {
   const app = express();
