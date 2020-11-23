9c37f6cc6ac348c8ddc95c0bbeadbc7939dbd805
Chris Ward <cdurianward@gmail.com>
2020-11-23 14:24:15 -0800

    Remove realm

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,5 @@
 const redisClient = require('./RedisClient');
 const prewarmCache = require('./prewarmCache');
-const RealmClient = require('./RealmClient');
 const { bugsnagApiKey, mongodbUri } = require('./environmentVariables');
 const { escapeRegExp } = require('./regex');
 const { ResourceList, ClassAPIResource } = require('./data');
@@ -9,7 +8,6 @@ module.exports = {
   bugsnagApiKey,
   mongodbUri,
   redisClient,
-  RealmClient,
   prewarmCache,
   escapeRegExp,
   ResourceList,
