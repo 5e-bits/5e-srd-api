ad18ba4689f8312167d831ac57a68e2ec6eff5fc
Chris Ward <cdurianward@gmail.com>
2020-11-30 13:38:17 -0800

    Magic Item

--- src/models/magicItem.js
+++ src/models/magicItem.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const MagicItemSchema = new Schema({
-  _id: {
-    type: String,
-    select: false
-  },
+const MagicItemEquipmentCategory = new Schema({
   index: String,
   name: String,
   url: String
 });
 
-module.exports = mongoose.model('MagicItem', MagicItemSchema, 'magic-items');
+const MagicItem = new Schema({
+  _id: {
+    type: String,
+    select: false
+  },
+  desc: [String],
+  equipment_category: MagicItemEquipmentCategory,
+  index: String,
+  name: String,
+  url: String
+});
+
+module.exports = mongoose.model('MagicItem', MagicItem, 'magic-items');
