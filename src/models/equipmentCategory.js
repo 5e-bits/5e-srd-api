085340fed93e456df415b908490d4bbdbb10a07e
Chris Ward <cdurianward@gmail.com>
2020-11-30 13:39:11 -0800

    Equipment Category

--- src/models/equipmentCategory.js
+++ src/models/equipmentCategory.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const EquipmentCategorySchema = new Schema({
+const EquipmentCategoryEquipment = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+const EquipmentCategory = new Schema({
   _id: {
     type: String,
     select: false
   },
+  equipment: [EquipmentCategoryEquipment],
   index: String,
   name: String,
   url: String
 });
 
-module.exports = mongoose.model(
-  'EquipmentCategory',
-  EquipmentCategorySchema,
-  'equipment-categories'
-);
+module.exports = mongoose.model('EquipmentCategory', EquipmentCategory, 'equipment-categories');
