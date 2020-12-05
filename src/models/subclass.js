0dd43c37c7c4f5da0b8149ad519bcc0a1cabd7c3
Chris Ward <cdurianward@gmail.com>
2020-12-04 18:49:46 -0800

    Add subclasses to mongoose

--- src/models/subclass.js
+++ src/models/subclass.js
@@ -1,14 +1,43 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var SubclassSchema = new Schema({
-  _id: {
-    type: String,
-    select: false
-  },
+const SubclassClass = new Schema({
   index: String,
   name: String,
   url: String
 });
 
-module.exports = mongoose.model('Subclass', SubclassSchema, 'subclasses');
+const SubclassSpellPrerequisite = new Schema({
+  index: String,
+  name: String,
+  type: String,
+  url: String
+});
+
+const SubclassSpellSpell = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SubclassSpell = new Schema({
+  prerequisites: [SubclassSpellPrerequisite],
+  spell: SubclassSpellSpell
+});
+
+const Subclass = new Schema({
+  _id: {
+    type: String,
+    select: false
+  },
+  class: SubclassClass,
+  desc: [String],
+  index: String,
+  name: String,
+  spells: [SubclassSpell],
+  subclass_flavor: String,
+  subclass_levels: String,
+  url: String
+});
+
+module.exports = mongoose.model('Subclass', Subclass, 'subclasses');
