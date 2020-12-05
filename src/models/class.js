690b3fcc7f1f8d481dd4baa2656bd4f7ac9cf0b6
Chris Ward <cdurianward@gmail.com>
2020-12-04 16:58:06 -0800

    Class mongoose

--- src/models/class.js
+++ src/models/class.js
@@ -1,15 +1,70 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ClassSchema = new Schema({
+const ClassProficiencyChoiceFrom = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const ClassProficiencyChoice = new Schema({
+  choose: Number,
+  from: [ClassProficiencyChoiceFrom],
+  type: String,
+});
+
+const ClassProficiency = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const ClassSavingThrow = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const ClassSpellcastingInfo = new Schema({
+  desc: [String],
+  name: String,
+});
+
+const ClassSpellcastingSpellcastingAbility = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const ClassSpellcasting = new Schema({
+  info: [ClassSpellcastingInfo],
+  level: Number,
+  spellcasting_ability: ClassSpellcastingSpellcastingAbility,
+});
+
+const ClassSubclass = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const Class = new Schema({
   _id: {
     type: String,
     select: false,
   },
+  class_levels: String,
+  hit_die: Number,
   index: String,
   name: String,
-  spellcasting: Object,
+  proficiencies: [ClassProficiency],
+  proficiency_choices: [ClassProficiencyChoice],
+  saving_throws: [ClassSavingThrow],
+  spellcasting: ClassSpellcasting,
+  spells: String,
+  starting_equipment: String,
+  subclasses: [ClassSubclass],
   url: String,
 });
 
-module.exports = mongoose.model('Class', ClassSchema, 'classes');
+module.exports = mongoose.model('Class', Class, 'classes');
