6a590757fcd790905145ec0ddef3974cc777515c
Chris Ward <cdurianward@gmail.com>
2020-12-07 11:48:58 -0800

    Some more mongoose (#162)
    * Add subraces to mongoose
    
    * Add subclasses to mongoose
    
    * Add seplls to mongoose
    
    * Add races to mongoose
    
    * Add features to mongoose

--- src/models/spell.js
+++ src/models/spell.js
@@ -1,18 +1,76 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var SpellSchema = new Schema({
+const SpellAreaOfEffect = new Schema({
+  size: Number,
+  type: String
+});
+
+const SpellClass = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SpellDamageDamageType = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SpellDamage = new Schema({
+  damage_type: SpellDamageDamageType
+});
+
+const SpellDcDcType = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SpellDc = new Schema({
+  dc_success: String,
+  dc_type: SpellDcDcType,
+  desc: String
+});
+
+const SpellSchool = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SpellSubclass = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const Spell = new Schema({
   _id: {
     type: String,
     select: false
   },
+  area_of_effect: SpellAreaOfEffect,
+  attack_type: String,
+  casting_time: String,
+  classes: [SpellClass],
+  components: [String],
+  concentration: Boolean,
+  damage: SpellDamage,
+  dc: SpellDc,
+  desc: [String],
+  duration: String,
+  higher_level: [String],
   index: String,
-  name: String,
   level: Number,
-  school: {
-    name: String
-  },
+  material: String,
+  name: String,
+  range: String,
+  ritual: Boolean,
+  school: SpellSchool,
+  subclasses: [SpellSubclass],
   url: String
 });
 
-module.exports = mongoose.model('Spell', SpellSchema, 'spells');
+module.exports = mongoose.model('Spell', Spell, 'spells');
--- src/models/subrace.js
+++ src/models/subrace.js
@@ -1,14 +1,74 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var SubraceSchema = new Schema({
-  _id: {
-    type: String,
-    select: false
-  },
+const SubraceAbilityBonusAbilityScore = new Schema({
   index: String,
   name: String,
   url: String
 });
 
-module.exports = mongoose.model('Subrace', SubraceSchema, 'subraces');
+const SubraceAbilityBonus = new Schema({
+  ability_score: SubraceAbilityBonusAbilityScore,
+  bonus: Number
+});
+
+const SubraceLanguageOptionFrom = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SubraceLanguageOption = new Schema({
+  choose: Number,
+  from: [SubraceLanguageOptionFrom],
+  type: String
+});
+
+const SubraceRace = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SubraceRacialTrait = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SubraceRacialTraitOptionFrom = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const SubraceRacialTraitOption = new Schema({
+  choose: Number,
+  from: [SubraceRacialTraitOptionFrom],
+  type: String
+});
+
+const SubraceStartingProficiency = new Schema({
+  index: String,
+  name: String,
+  url: String
+});
+
+const Subrace = new Schema({
+  _id: {
+    type: String,
+    select: false
+  },
+  ability_bonuses: [SubraceAbilityBonus],
+  desc: String,
+  index: String,
+  language_options: SubraceLanguageOption,
+  name: String,
+  race: SubraceRace,
+  racial_trait_options: SubraceRacialTraitOption,
+  racial_traits: [SubraceRacialTrait],
+  starting_proficiencies: [SubraceStartingProficiency],
+  url: String
+});
+
+module.exports = mongoose.model('Subrace', Subrace, 'subraces');
