746e9afd36abd215165fea41a926aa91bd665036
Christopher Ward <cdurianward@gmail.com>
2020-12-06 17:11:31 -0800

    Silencio

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,5 +1,6 @@
 const config = require('./jest.config');
 config.testRegex = '\\.itest\\.js$'; //Overriding testRegex option
 config.runInBand = true;
+config.silent = true;
 console.log('RUNNING INTEGRATION TESTS');
 module.exports = config;
