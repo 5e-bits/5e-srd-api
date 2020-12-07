744346c77d070d5718132b8b07cf02fa812e9138
Christopher Ward <cdurianward@gmail.com>
2020-12-06 16:53:26 -0800

    Switch to runInBand?

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,4 +1,5 @@
 const config = require('./jest.config');
 config.testRegex = '\\.itest\\.js$'; //Overriding testRegex option
+config.runInBand = true;
 console.log('RUNNING INTEGRATION TESTS');
 module.exports = config;
