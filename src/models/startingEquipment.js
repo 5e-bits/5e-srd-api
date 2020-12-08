5beb3959f0b9d9e3ceb00bcb9e0ab5638c53658b
Chris Ward <cdurianward@gmail.com>
2020-12-08 12:37:50 -0800

    Starting Equipment

--- src/models/startingEquipment.js
+++ src/models/startingEquipment.js
@@ -1,17 +1,50 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var StartingEquipmentSchema = new Schema({
+const StartingequipmentClass = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const StartingequipmentStartingEquipmentEquipment = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const StartingequipmentStartingEquipment = new Schema({
+  equipment: StartingequipmentStartingEquipmentEquipment,
+  quantity: Number,
+});
+
+const StartingequipmentStartingEquipmentOptionFromEquipment = new Schema({
+  index: String,
+  name: String,
+  url: String,
+});
+
+const StartingequipmentStartingEquipmentOptionFrom = new Schema({
+  equipment: StartingequipmentStartingEquipmentOptionFromEquipment,
+  quantity: Number,
+});
+
+const StartingequipmentStartingEquipmentOption = new Schema({
+  choose: Number,
+  from: [StartingequipmentStartingEquipmentOptionFrom],
+  type: String,
+});
+
+const StartingEquipment = new Schema({
   _id: {
     type: String,
     select: false,
   },
+  class: StartingequipmentClass,
   index: String,
-  class: {
-    name: String,
-    url: String,
-  },
+  starting_equipment: [StartingequipmentStartingEquipment],
+  starting_equipment_options: [StartingequipmentStartingEquipmentOption],
   url: String,
 });
 
-module.exports = mongoose.model('StartingEquipment', StartingEquipmentSchema, 'startingequipment');
+module.exports = mongoose.model('StartingEquipment', StartingEquipment, 'startingequipment');
