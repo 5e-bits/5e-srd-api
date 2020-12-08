255b18be5d7b97fadc317b16d92eeeec61580cef
Chris Ward <cdurianward@gmail.com>
2020-12-08 12:38:54 -0800

    Remove _id

--- src/models/startingEquipment.js
+++ src/models/startingEquipment.js
@@ -2,34 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const StartingequipmentClass = new Schema({
+  _id: false,
   index: String,
   name: String,
   url: String,
 });
 
 const StartingequipmentStartingEquipmentEquipment = new Schema({
+  _id: false,
   index: String,
   name: String,
   url: String,
 });
 
 const StartingequipmentStartingEquipment = new Schema({
+  _id: false,
   equipment: StartingequipmentStartingEquipmentEquipment,
   quantity: Number,
 });
 
 const StartingequipmentStartingEquipmentOptionFromEquipment = new Schema({
+  _id: false,
   index: String,
   name: String,
   url: String,
 });
 
 const StartingequipmentStartingEquipmentOptionFrom = new Schema({
+  _id: false,
   equipment: StartingequipmentStartingEquipmentOptionFromEquipment,
   quantity: Number,
 });
 
 const StartingequipmentStartingEquipmentOption = new Schema({
+  _id: false,
   choose: Number,
   from: [StartingequipmentStartingEquipmentOptionFrom],
   type: String,
