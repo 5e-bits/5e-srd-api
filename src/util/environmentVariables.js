c04ea9707e91179f994db04c32ac9ae8c7478886
Christopher Ward <cdurianward@gmail.com>
2021-01-15 15:11:13 -0800

    Security updates

--- src/util/environmentVariables.js
+++ src/util/environmentVariables.js
@@ -1,13 +1,9 @@
 const redisUrl = process.env.REDIS_URL || '';
 const bugsnagApiKey = process.env.BUGSNAG_API_KEY || 'local';
 const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost/5e-database';
-const realmAppId = process.env.REALM_APP_ID || '';
-const realmApiKey = process.env.REALM_API_KEY || '';
 
 module.exports = {
   redisUrl,
   bugsnagApiKey,
   mongodbUri,
-  realmAppId,
-  realmApiKey,
 };
