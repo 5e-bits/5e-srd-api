db454543fa38e3c6659b2885c71304b78ae2aaf0
Dan Lawrence <daniellawrence2468@gmail.com>
2021-02-16 21:36:49 +0000

    Remove starting equipment, and suggested characteristics endpoints from background controller.

--- src/controllers/api/backgroundController.js
+++ src/controllers/api/backgroundController.js
@@ -1,6 +1,4 @@
 const Background = require('../../models/background');
-const StartingEquipment = require('../../models/startingEquipment');
-const SuggestedCharacteristics = require('../../models/suggestedCharacteristics');
 const Feature = require('../../models/feature');
 const SimpleController = require('../simpleController');
 
@@ -8,30 +6,6 @@ const simpleController = new SimpleController(Background);
 
 exports.index = (req, res, next) => simpleController.index(req, res, next);
 exports.show = (req, res, next) => simpleController.show(req, res, next);
-
-exports.showStartingEquipmentForBackground = (req, res, next) => {
-  const urlString = '/api/backgrounds/' + req.params.index;
-
-  return StartingEquipment.findOne({ 'background.url': urlString })
-    .then(data => {
-      res.status(200).json(data);
-    })
-    .catch(err => {
-      next(err);
-    });
-};
-
-exports.showSuggestedCharacteristicsForBackground = (req, res, next) => {
-  const urlString = '/api/backgrounds/' + req.params.index;
-
-  return SuggestedCharacteristics.findOne({ 'background.url': urlString })
-    .then(data => {
-      res.status(200).json(data);
-    })
-    .catch(err => {
-      next(err);
-    });
-};
 
 exports.showFeatureForBackground = (req, res, next) => {
   const urlString = '/api/backgrounds/' + req.params.index;
