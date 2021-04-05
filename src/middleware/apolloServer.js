8c6535724824153fe5f8fbef7dd6a7f92b6c5125
Chris Ward <cdurianward@gmail.com>
2021-04-05 09:11:45 -0700

    Adding apollo caching because I can (#201)

--- src/middleware/apolloServer.js
+++ src/middleware/apolloServer.js
@@ -1,8 +1,15 @@
 const { ApolloServer } = require('apollo-server-express');
+const responseCachePlugin = require('apollo-server-plugin-response-cache');
 const schema = require('../graphql/schema');
 
 const createApolloMiddleware = async () => {
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({
+    schema,
+    plugins: [responseCachePlugin()],
+    cacheControl: {
+      defaultMaxAge: 3600, // 1 hour
+    },
+  });
   return server;
 };
 
