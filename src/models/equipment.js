f280307989dd627a30b7d8fdf6355359b3097bb8
Christopher Ward <cdurianward@gmail.com>
2021-06-17 17:44:51 -0700

    Equipment

--- src/models/equipment.js
+++ src/models/equipment.js
@@ -2,87 +2,79 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { APIReference } = require('./common');
 
-const ArmorClass = new Schema({
-  _id: false,
-  base: Number,
-  dex_bonus: Boolean,
-  max_bonus: Number,
-});
+const ArmorClass = {
+  base: { type: Number, index: true },
+  dex_bonus: { type: Boolean, index: true },
+  max_bonus: { type: Number, index: true },
+};
 
-const Content = new Schema({
-  _id: false,
+const Content = {
   item: APIReference,
-  quantity: Number,
-});
+  quantity: { type: Number, index: true },
+};
 
-const Cost = new Schema({
-  _id: false,
-  quantity: Number,
-  unit: String,
-});
+const Cost = {
+  quantity: { type: Number, index: true },
+  unit: { type: String, index: true },
+};
 
-const Damage = new Schema({
-  _id: false,
-  damage_dice: String,
+const Damage = {
+  damage_dice: { type: String, index: true },
   damage_type: APIReference,
-});
+};
 
-const Range = new Schema({
-  _id: false,
-  long: Number,
-  normal: Number,
-});
+const Range = {
+  long: { type: Number, index: true },
+  normal: { type: Number, index: true },
+};
 
-const Speed = new Schema({
-  _id: false,
-  quantity: Number,
-  unit: String,
-});
+const Speed = {
+  quantity: { type: Number, index: true },
+  unit: { type: String, index: true },
+};
 
-const ThrowRange = new Schema({
-  _id: false,
-  long: Number,
-  normal: Number,
-});
+const ThrowRange = {
+  long: { type: Number, index: true },
+  normal: { type: Number, index: true },
+};
 
-const TwoHandedDamage = new Schema({
-  _id: false,
-  damage_dice: String,
+const TwoHandedDamage = {
+  damage_dice: { type: String, index: true },
   damage_type: APIReference,
-});
+};
 
 const Equipment = new Schema({
   _id: {
     type: String,
     select: false,
   },
-  armor_category: String,
+  armor_category: { type: String, index: true },
   armor_class: ArmorClass,
-  capacity: String,
-  category_range: String,
+  capacity: { type: String, index: true },
+  category_range: { type: String, index: true },
   contents: [Content],
   cost: Cost,
   damage: Damage,
-  desc: [String],
+  desc: { type: [String], index: true },
   equipment_category: APIReference,
   gear_category: APIReference,
-  index: String,
-  name: String,
+  index: { type: String, index: true },
+  name: { type: String, index: true },
   properties: [APIReference],
-  quantity: Number,
+  quantity: { type: Number, index: true },
   range: Range,
-  special: [String],
+  special: { type: [String], index: true },
   speed: Speed,
-  stealth_disadvantage: Boolean,
-  str_minimum: Number,
+  stealth_disadvantage: { type: Boolean, index: true },
+  str_minimum: { type: Number, index: true },
   throw_range: ThrowRange,
-  tool_category: String,
+  tool_category: { type: String, index: true },
   two_handed_damage: TwoHandedDamage,
-  url: String,
-  vehicle_category: String,
-  weapon_category: String,
-  weapon_range: String,
-  weight: Number,
+  url: { type: String, index: true },
+  vehicle_category: { type: String, index: true },
+  weapon_category: { type: String, index: true },
+  weapon_range: { type: String, index: true },
+  weight: { type: Number, index: true },
 });
 
 module.exports = mongoose.model('Equipment', Equipment, 'equipment');
