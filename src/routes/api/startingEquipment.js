6e0fe538b7cc143125542177dfbfb8ac12900669
Christopher Ward <cdurianward@gmail.com>
2021-07-19 21:37:16 -0700

    I believe that's everything.

--- src/routes/api/startingEquipment.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const StartingEquipmentController = require('../../controllers/api/startingEquipmentController');
-
-router.get('/', StartingEquipmentController.index);
-router.get('/:index', StartingEquipmentController.show);
-
-module.exports = router;
