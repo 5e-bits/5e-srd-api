6d84860f29f30f3d9855288d5518049728c8f181
abias <abias1122@gmail.com>
2021-08-07 14:58:20 -0400

    Fix lint errors

--- src/models/feat.js
+++ src/models/feat.js
@@ -4,16 +4,16 @@ const { APIReference } = require('./common');
 
 const Prerequisite = {
   ability_score: APIReference,
-  minimum_score: { type: Number, index: true }
+  minimum_score: { type: Number, index: true },
 };
 
 const Feat = new Schema({
-    _id: { type: String, select: false },
-    index: { type: String, index: true },
-    name: { type: String, index: true },
-    prerequisites: [Prerequisite],
-    desc: { type: [String], index: true},
-    url: { type: String, index: true }
+  _id: { type: String, select: false },
+  index: { type: String, index: true },
+  name: { type: String, index: true },
+  prerequisites: [Prerequisite],
+  desc: { type: [String], index: true },
+  url: { type: String, index: true },
 });
 
 module.exports = mongoose.model('Feat', Feat, 'feats');
