09d06c358fcc8367c9509f9eb59d7b9ae9d70e4e
Anthony Bias <abias1122@gmail.com>
2021-08-12 16:00:23 -0400

    Get started adding GraphQL Relations (#239)
    * Add levels, spells, and proficiencies relations to class
    
    * Add class reference to subclass
    
    * Add more relations
    
    * Add relations for traits
    
    * Add more relations
    
    * alphabetize relations, add more relations
    
    * Add froms relation to monsters
    
    * Comment out relations that lead to infinite loops during introspection
    
    * Fix lint errors

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -4,8 +4,8 @@ const schemaComposer = require('graphql-compose').schemaComposer;
 const AbilityScore = require('../models/abilityScore');
 const Alignment = require('../models/alignment');
 const Background = require('../models/background');
-const Class = require('../models/class');
 const Condition = require('../models/condition');
+const Class = require('../models/class');
 const DamageType = require('../models/damageType');
 const Equipment = require('../models/equipment');
 const EquipmentCategory = require('../models/equipmentCategory');
@@ -20,6 +20,7 @@ const Proficiency = require('../models/proficiency');
 const Race = require('../models/race');
 const Rule = require('../models/rule');
 const RuleSection = require('../models/ruleSection');
+const Skill = require('../models/skill');
 const Spell = require('../models/spell');
 const Subclass = require('../models/subclass');
 const Subrace = require('../models/subrace');
@@ -46,11 +47,337 @@ const ProficiencyTC = composeMongoose(Proficiency);
 const RaceTC = composeMongoose(Race);
 const RuleTC = composeMongoose(Rule);
 const RuleSectionTC = composeMongoose(RuleSection);
+const SkillTC = composeMongoose(Skill);
 const SpellTC = composeMongoose(Spell);
 const SubclassTC = composeMongoose(Subclass);
 const SubraceTC = composeMongoose(Subrace);
 const TraitTC = composeMongoose(Trait);
 const WeaponPropertyTC = composeMongoose(WeaponProperty);
+
+// TODO: Figure out how to use commented out relations without breaking GraphQL Playground.
+// Commented out relations lead to circular dependencies. This causes graphql introspection to enter an infinite recursive loop,
+// breaking GraphQL Playground.
+
+AbilityScoreTC.addRelation('skills', {
+  resolver: () => SkillTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.skills.map(skill => skill.index) },
+      },
+    }),
+  },
+  projection: { skills: true },
+});
+
+BackgroundTC.addRelation('starting_proficiencies', {
+  resolver: () => ProficiencyTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.starting_proficiencies.map(prof => prof.index) },
+      },
+    }),
+  },
+  projection: { starting_proficiencies: true },
+});
+
+ClassTC.addRelation('class_levels', {
+  resolver: () => LevelTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      class: {
+        index: source.index,
+      },
+    }),
+  },
+  projection: { index: true },
+});
+
+ClassTC.addRelation('spells', {
+  resolver: () => SpellTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      classes: [{ index: source.index }],
+    }),
+  },
+  projection: { index: true },
+});
+
+ClassTC.addRelation('proficiencies', {
+  resolver: () => ProficiencyTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      classes: [{ index: source.index }],
+    }),
+  },
+  projection: { index: true },
+});
+
+// EquipmentTC.addRelation('equipment_category', {
+//   resolver: () => EquipmentCategoryTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs : {
+//     filter: source => ({
+//       index: source.equipment_category.index
+//     })
+//   },
+//   projection: { equipment_category: true }
+// });
+
+// EquipmentTC.addRelation('gear_category', {
+//   resolver: () => EquipmentCategoryTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs : {
+//     filter: source => ({
+//       index: source.gear_category.index
+//     })
+//   },
+//   projection: { gear_category: true }
+// });
+
+EquipmentTC.addRelation('properties', {
+  resolver: () => WeaponPropertyTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.properties.map(prop => prop.index) },
+      },
+    }),
+  },
+  projection: { properties: true },
+});
+
+EquipmentCategoryTC.addRelation('equipment', {
+  resolver: () => EquipmentTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.equipment.map(e => e.index) },
+      },
+    }),
+  },
+  projection: { equipment: true },
+});
+
+// FeatureTC.addRelation('level', {
+//   resolver: () => LevelTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       features: [{ index: source.index }]
+//     })
+//   },
+//   projection: { index: true }
+// });
+
+FeatureTC.addRelation('class', {
+  resolver: () => ClassTC.mongooseResolvers.findOne(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      index: source.class.index,
+    }),
+  },
+  projection: { class: true },
+});
+
+FeatureTC.addRelation('subclass', {
+  resolver: () => SubclassTC.mongooseResolvers.findOne(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      index: source.subclass.index,
+    }),
+  },
+  projection: { subclass: true },
+});
+
+// LevelTC.addRelation('class', {
+//   resolver: () => ClassTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs: {
+//       filter: source => ({
+//           index: source.class.index
+//       })
+//   },
+//   projection: { class: true }
+// });
+
+// LevelTC.addRelation('subclass', {
+//   resolver: () => SubclassTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs: {
+//       filter: source => ({
+//           index: source.subclass.index
+//       })
+//   },
+//   projection: { subclass: true }
+// });
+
+LevelTC.addRelation('features', {
+  resolver: () => FeatureTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      level: source.level,
+      class: { index: source.class.index },
+    }),
+  },
+  projection: { level: true, class: true },
+});
+
+RaceTC.addRelation('traits', {
+  resolver: () => TraitTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.traits.map(t => t.index) },
+      },
+    }),
+  },
+  projection: { traits: true },
+});
+
+RuleTC.addRelation('subsections', {
+  resolver: RuleSectionTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.subsections.map(s => s.index) },
+      },
+    }),
+  },
+  projection: { subsections: true },
+});
+
+// MonsterTC.addRelation('forms', {
+//   resolver: () => MonsterTC.mongooseResolvers.findMany(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       _operators: {
+//         url: {in: source.forms.map(f => f.url)}
+//       }
+//     })
+//   },
+//   projection: { forms: true }
+// });
+
+// SkillTC.addRelation('ability_score', {
+//   resolver: () => AbilityScoreTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       index: source.ability_score.index
+//     })
+//   },
+//   projection: { ability_score: true }
+// });
+
+// SpellTC.addRelation('classes', {
+//   resolver: () => ClassTC.mongooseResolvers.findMany(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       _operators: {
+//         index: {in: source.classes.map(c => c.index)}
+//       }
+//     })
+//   },
+//   projection: { classes: true }
+// });
+
+// SpellTC.addRelation('subclasses', {
+//   resolver: () => SubclassTC.mongooseResolvers.findMany(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       _operators: {
+//         index: {in: source.subclasses.map(c => c.index)}
+//       }
+//     })
+//   },
+//   projection: { subclasses: true }
+// });
+
+SpellTC.addRelation('school', {
+  resolver: () => MagicSchoolTC.mongooseResolvers.findOne(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      index: source.school.index,
+    }),
+  },
+  projection: { school: true },
+});
+
+// SubclassTC.addRelation('class', {
+//   resolver: () => ClassTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs: {
+//       filter: source => ({
+//           index: source.class.index
+//       })
+//   },
+//   projection: { class: true }
+// });
+
+SubclassTC.addRelation('subclass_levels', {
+  resolver: () => LevelTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      subclass: {
+        index: source.index,
+      },
+    }),
+    projection: { index: true },
+  },
+});
+
+SubraceTC.addRelation('racial_traits', {
+  resolver: () => TraitTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.racial_traits.map(t => t.index) },
+      },
+    }),
+  },
+  projection: { racial_traits: true },
+});
+
+// TraitTC.addRelation('races', {
+//   resolver: () => RaceTC.mongooseResolvers.findMany(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       _operators: {
+//         index: {in: source.races.map(race => race.index)}
+//       }
+//     })
+//   },
+//   projection: { races: true }
+// });
+
+// TraitTC.addRelation('subraces', {
+//   resolver: () => SubraceTC.mongooseResolvers.findMany(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       _operators: {
+//         index: {in: source.subraces.map(race => race.index)}
+//       }
+//     })
+//   },
+//   projection: { subraces: true }
+// });
+
+TraitTC.addRelation('proficiencies', {
+  resolver: () => ProficiencyTC.mongooseResolvers.findMany(customizationOptions),
+  prepareArgs: {
+    filter: source => ({
+      _operators: {
+        index: { in: source.proficiencies.map(prof => prof.index) },
+      },
+    }),
+  },
+  projection: { proficiencies: true },
+});
+
+// TraitTC.addRelation('parent', {
+//   resolver: () => TraitTC.mongooseResolvers.findOne(customizationOptions),
+//   prepareArgs: {
+//     filter: source => ({
+//       index: source.parent.index
+//     })
+//   },
+//   projection: { parent: true }
+// });
 
 schemaComposer.Query.addFields({
   abilityScore: AbilityScoreTC.mongooseResolvers.findOne(customizationOptions),
@@ -91,6 +418,8 @@ schemaComposer.Query.addFields({
   rules: RuleTC.mongooseResolvers.findMany(customizationOptions),
   ruleSection: RuleSectionTC.mongooseResolvers.findOne(customizationOptions),
   ruleSections: RuleSectionTC.mongooseResolvers.findMany(customizationOptions),
+  skill: SkillTC.mongooseResolvers.findOne(customizationOptions),
+  skills: SkillTC.mongooseResolvers.findMany(customizationOptions),
   spell: SpellTC.mongooseResolvers.findOne(customizationOptions),
   spells: SpellTC.mongooseResolvers.findMany(customizationOptions),
   subclass: SubclassTC.mongooseResolvers.findOne(customizationOptions),
