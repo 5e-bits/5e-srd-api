2654fe83a216da7939b2b7cf337c619de78b4e01
abias <abias1122@gmail.com>
2021-08-27 18:14:42 -0400

    Add subraces to traits

--- src/graphql/resolvers/traitResolver.js
+++ src/graphql/resolvers/traitResolver.js
@@ -1,5 +1,6 @@
 const ProficiencyModel = require('../../models/proficiency');
 const RaceModel = require('../../models/race');
+const Subrace = require('../../models/subrace');
 const TraitModel = require('../../models/trait');
 
 const Trait = {
@@ -7,6 +8,8 @@ const Trait = {
     await ProficiencyModel.find({ index: { $in: trait.proficiencies.map(p => p.index) } }).lean(),
   parent: async trait =>
     trait.parent ? await TraitModel.findOne({ index: trait.parent.index }).lean() : null,
+  subraces: async trait =>
+    await Subrace.find({ index: { $in: trait.subraces.map(s => s.index) } }).lean(),
   races: async trait =>
     await RaceModel.find({ index: { $in: trait.races.map(r => r.index) } }).lean(),
 };
