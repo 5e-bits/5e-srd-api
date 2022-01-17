50d12322b3c515722a2d4335805867087b09b093
abias <abias1122@gmail.com>
2022-01-17 03:55:18 -0500

    Add order to equipment category equipment

--- src/graphql/resolvers/equipmentCategoryResolver.js
+++ src/graphql/resolvers/equipmentCategoryResolver.js
@@ -1,12 +1,44 @@
 const Equipment = require('../../models/equipment');
 const MagicItem = require('../../models/magicItem');
+const { coalesceSort } = require('./common');
 
 const EquipmentCategory = {
-  equipment: async equipmentCategory => {
+  equipment: async (equipmentCategory, args) => {
     const indexes = equipmentCategory.equipment.map(e => e.index);
     const equipment = await Equipment.find({ index: { $in: indexes } }).lean();
     const magicItems = await MagicItem.find({ index: { $in: indexes } }).lean();
-    return equipment.concat(magicItems);
+    const equipmentToReturn = equipment.concat(magicItems);
+
+    let sort = {};
+    if (args.order) {
+      sort = coalesceSort(args.order, value => value.toLowerCase());
+    }
+
+    if (sort.name) {
+      equipmentToReturn.sort((a, b) => {
+        if (a.name < b.name) {
+          return -sort.name;
+        } else if (a.name > b.name) {
+          return sort.name;
+        } else {
+          return 0;
+        }
+      });
+    }
+
+    if (sort.weight) {
+      equipmentToReturn.sort((a, b) => {
+        if (a.weight < b.weight) {
+          return -sort.weight;
+        } else if (a.weight > b.weight) {
+          return sort.weight;
+        } else {
+          return 0;
+        }
+      });
+    }
+
+    return equipmentToReturn;
   },
 };
 
