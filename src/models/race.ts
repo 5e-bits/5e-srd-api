f9c9b8ec9d6dedfc3b78272ba932ce07be922ab8
Christopher Ward <cdurianward@gmail.com>
2022-03-01 20:11:30 -0800

    Races

--- src/models/race.ts
+++ src/models/race.ts
@@ -1,10 +1,21 @@
 import * as mongoose from 'mongoose';
-import { APIReference } from './common';
+import { APIReference, APIReferenceSchema } from './common';
+
+interface AbilityBonusOption {
+  ability_score: APIReference;
+  bonus: number;
+}
 
 const AbilityBonusOption = {
-  ability_score: APIReference,
+  ability_score: APIReferenceSchema,
   bonus: { type: Number, index: true },
 };
+
+interface RaceAbilityBonusOptions {
+  choose: number;
+  from: AbilityBonusOption[];
+  type: string;
+}
 
 const RaceAbilityBonusOptions = {
   choose: { type: Number, index: true },
@@ -12,24 +23,62 @@ const RaceAbilityBonusOptions = {
   type: { type: String, index: true },
 };
 
+interface RaceAbilityBonus {
+  ability_score: APIReference;
+  bonus: number;
+}
+
 const RaceAbilityBonus = {
-  ability_score: APIReference,
+  ability_score: APIReferenceSchema,
   bonus: { type: Number, index: true },
 };
 
+interface LanguageOptions {
+  choose: number;
+  from: APIReference[];
+  type: string;
+}
+
 const LanguageOptions = {
   choose: { type: Number, index: true },
-  from: [APIReference],
+  from: [APIReferenceSchema],
   type: { type: String, index: true },
 };
+
+interface StartingProficiencyOptions {
+  choose: number;
+  from: APIReference[];
+  type: string;
+}
 
 const StartingProficiencyOptions = {
   choose: { type: Number, index: true },
-  from: [APIReference],
+  from: [APIReferenceSchema],
   type: { type: String, index: true },
 };
 
-const Race = new mongoose.Schema({
+interface Race {
+  _id?: string;
+  ability_bonus_options?: RaceAbilityBonusOptions;
+  ability_bonuses: RaceAbilityBonus[];
+  age: string;
+  alignment: string;
+  index: string;
+  language_desc: string;
+  language_options: LanguageOptions;
+  languages: APIReference[];
+  name: string;
+  size: string;
+  size_description: string;
+  speed: number;
+  starting_proficiencies: APIReference[];
+  starting_proficiency_options?: StartingProficiencyOptions;
+  subraces: APIReference[];
+  traits: APIReference[];
+  url: string;
+}
+
+const Race = new mongoose.Schema<Race>({
   _id: { type: String, select: false },
   ability_bonus_options: RaceAbilityBonusOptions,
   ability_bonuses: [RaceAbilityBonus],
@@ -38,16 +87,16 @@ const Race = new mongoose.Schema({
   index: { type: String, index: true },
   language_desc: { type: String, index: true },
   language_options: LanguageOptions,
-  languages: [APIReference],
+  languages: [APIReferenceSchema],
   name: { type: String, index: true },
   size: { type: String, index: true },
   size_description: { type: String, index: true },
   speed: { type: Number, index: true },
-  starting_proficiencies: [APIReference],
+  starting_proficiencies: [APIReferenceSchema],
   starting_proficiency_options: StartingProficiencyOptions,
-  subraces: [APIReference],
-  traits: [APIReference],
+  subraces: [APIReferenceSchema],
+  traits: [APIReferenceSchema],
   url: { type: String, index: true },
 });
 
-export default mongoose.model('Race', Race, 'races');
+export default mongoose.model<Race>('Race', Race, 'races');
