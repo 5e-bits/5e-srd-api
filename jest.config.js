675df7d1f49b65288019b2aeae84e8fa30324516
Christopher Ward <cdurianward@gmail.com>
2022-03-05 20:38:16 -0800

    That fixes the unit tests at least

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
   testRegex: 'test\\.js$',
+  modulePathIgnorePatterns: ['/dist/'],
   coveragePathIgnorePatterns: ['/node_modules/'],
 };
