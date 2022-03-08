5687b62dac955f8984151f4073d3f712b5ab7a18
Christopher Ward <cdurianward@gmail.com>
2022-03-07 17:08:03 -0800

    Let's see if Github works?

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -1,5 +1,4 @@
 import config from './jest.config';
 config.testRegex = '\\.itest\\.(js|ts)$'; //Overriding testRegex option
-config.silent = true;
 console.log('RUNNING INTEGRATION TESTS');
 export default config;
