fd998a0dc93a969549eccb7d08510318313207d6
Christopher Ward <cdurianward@gmail.com>
2022-03-14 14:26:04 -0700

    Eventually get everything

--- src/models/abilityScore/index.ts
+++ src/models/abilityScore/index.ts
@@ -12,4 +12,4 @@ const AbilityScoreSchema = new Schema<AbilityScore>({
   url: { type: String, index: true },
 });
 
-module.exports = model('AbilityScore', AbilityScoreSchema, 'ability-scores');
+module.exports = model<AbilityScore>('AbilityScore', AbilityScoreSchema, 'ability-scores');
