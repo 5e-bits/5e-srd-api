54fd2fa933a93f041689a2a09e60ffba664fc545
Eric <ecshreve13@gmail.com>
2022-03-26 08:51:41 -0700

    ecshreve: [openapi] - small tweaks to bring docs in line with prod (#288)
    * openapi: cleanup character data paths
    
    * openapi: class cleanup
    
    * openapi: tweaks to bring docs in line with actual api behavior
    
    * controller / docs: fix 404 for classes/index/spellcasting

--- src/controllers/api/classController.js
+++ src/controllers/api/classController.js
@@ -86,7 +86,11 @@ exports.showStartingEquipmentForClass = async (req, res, next) => {
 exports.showSpellcastingForClass = async (req, res, next) => {
   try {
     const data = await Class.findOne({ index: req.params.index });
-    return res.status(200).json(data.spellcasting);
+    if (data && data.spellcasting) {
+      return res.status(200).json(data.spellcasting);
+    } else {
+      return res.status(404).json({ error: 'Not found' });
+    }
   } catch (err) {
     next(err);
   }
