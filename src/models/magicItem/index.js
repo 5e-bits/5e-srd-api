30b9ab61e3baeae165210c7b40ef069792e94071
Matt Loberg <mloberg@users.noreply.github.com>
2022-03-28 18:15:39 -0500

    feat(magic-item): update schema for magic items
    Adds rarity and variants to magic items
    
    re 5e-bits/5e-database#455

--- src/models/magicItem/index.js
+++ src/models/magicItem/index.js
@@ -2,13 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { APIReference } = require('../common');
 
+const Rarity = new Schema({
+  _id: false,
+  name: { type: String, index: true },
+});
+
 const MagicItem = new Schema({
   _id: { type: String, select: false },
   desc: { type: [String], index: true },
   equipment_category: APIReference,
   index: { type: String, index: true },
   name: { type: String, index: true },
+  rarity: Rarity,
   url: { type: String, index: true },
+  variants: [APIReference],
 });
 
 module.exports = mongoose.model('MagicItem', MagicItem, 'magic-items');
