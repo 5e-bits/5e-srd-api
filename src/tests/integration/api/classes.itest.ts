8c30270623cc28419d020dcaed69bc5ac6e58968
Christopher Ward <cdurianward@gmail.com>
2022-04-01 11:53:13 -0700

    Adjust integ test

--- src/tests/integration/api/classes.itest.ts
+++ src/tests/integration/api/classes.itest.ts
@@ -141,7 +141,8 @@ describe('/api/classes', () => {
       it('returns the subclass levels as well', async () => {
         const indexRes = await request(app).get('/api/classes');
         const index = indexRes.body.results[1].index;
-        const subclass = indexRes.body.results[1].subclasses[0].index;
+        const classRes = await request(app).get(`/api/classes/${index}`);
+        const subclass = classRes.body.results[1].subclasses[0].index;
         const res = await request(app).get(`/api/classes/${index}/levels?subclass=${subclass}`);
         expect(res.statusCode).toEqual(200);
         expect(res.body.length).not.toEqual(0);
