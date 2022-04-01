4323471238ed457870dc06f69c5a9b1d2719df31
Matt Loberg <mloberg@users.noreply.github.com>
2022-04-01 16:51:23 +0000

    feat(magic-item): update schema for magic items (#293)
    * feat(magic-item): update schema for magic items
    
    Adds rarity and variants to magic items
    
    re 5e-bits/5e-database#455
    
    * Apply suggestions from code review
    
    Just so I  can merge this in.
    
    Co-authored-by: Chris Ward <cdurianward@gmail.com>

--- src/models/magicItem/index.js
+++ src/models/magicItem/index.js
@@ -2,13 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { APIReference } = require('../common');
 
+const Rarity = new Schema({
+  _id: false,
+  name: { type: String, index: true },
+});
+
 const MagicItem = new Schema({
   _id: { type: String, select: false },
   desc: { type: [String], index: true },
   equipment_category: APIReference,
   index: { type: String, index: true },
   name: { type: String, index: true },
+  rarity: Rarity,
   url: { type: String, index: true },
+  variants: [APIReference],
+  variant: Boolean,
 });
 
 module.exports = mongoose.model('MagicItem', MagicItem, 'magic-items');
--- src/models/magicItem/types.d.ts
+++ src/models/magicItem/types.d.ts
@@ -1,11 +1,19 @@
 import * as mongoose from 'mongoose';
 import { APIReference } from '../common/types';
 
+interface Rarity {
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+}
+
 export type MagicItem = {
   _id?: mongoose.Types.ObjectId;
   desc: string[];
   equipment_category: APIReference;
   index: string;
   name: string;
+  rarity: Rarity;
   url: string;
+  variants: APIReference[];
+  variant: boolean;
 };
