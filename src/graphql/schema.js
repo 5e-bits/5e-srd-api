9513cd3fccfc51649e38a81e7bfe26e9f02eed6f
abias <abias1122@gmail.com>
2022-04-14 09:05:48 -0400

    Fix bug loading typeDefs in schema

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -3,7 +3,10 @@ const path = require('path');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const resolvers = require('./resolvers/rootResolvers');
 
-const typeDefs = readFileSync(path.resolve(__dirname, 'typeDefs.graphql'), 'UTF-8');
+const typeDefs = readFileSync(
+  path.resolve(process.cwd(), 'src', 'graphql', 'typeDefs.graphql'),
+  'UTF-8'
+);
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 module.exports = schema;
