f6c976fe8819342bc01dcb6312873ddf375ee37a
abias <abias1122@gmail.com>
2022-06-05 01:23:29 -0400

    Add rarity to magic items

--- src/graphql/resolvers/magicItemResolver.js
+++ src/graphql/resolvers/magicItemResolver.js
@@ -2,6 +2,7 @@ const { equipmentBaseFieldResolvers } = require('./common');
 
 const MagicItem = {
   ...equipmentBaseFieldResolvers,
+  rarity: async magicItem => magicItem.rarity.name.toUpperCase().replace(' ', '_'),
 };
 
 module.exports = MagicItem;
