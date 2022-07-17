920248afefb30e59510cd647fcc6e874e2b087b9
Anthony Bias <abias1122@gmail.com>
2022-07-16 22:40:41 -0400

    Fix schema

--- src/models/background/index.js
+++ src/models/background/index.js
@@ -2,30 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { APIReference, Choice } = require('../common');
 
-const LanguageOptions = new Schema({
-  _id: false,
-  choose: { type: Number, index: true },
-  from: [APIReference],
-  type: { type: String, index: true },
-});
-
 const Equipment = new Schema({
   _id: false,
   equipment: APIReference,
   quantity: { type: Number, index: true },
-});
-
-const Ideal = new Schema({
-  _id: false,
-  desc: { type: String, index: true },
-  alignments: [APIReference],
-});
-
-const IdealOptions = new Schema({
-  _id: false,
-  choose: { type: Number, index: true },
-  from: [Ideal],
-  type: { type: String, index: true },
 });
 
 const Feature = new Schema({
@@ -39,13 +19,13 @@ const Background = new Schema({
   index: { type: String, index: true },
   name: { type: String, index: true },
   starting_proficiencies: [APIReference],
-  language_options: LanguageOptions,
+  language_options: Choice,
   url: { type: String, index: true },
   starting_equipment: [Equipment],
   starting_equipment_options: { type: [Choice], index: true },
   feature: Feature,
   personality_traits: Choice,
-  ideals: IdealOptions,
+  ideals: Choice,
   bonds: Choice,
   flaws: Choice,
 });
