fa9e69cc8dbb3c941df82ad7074c3f9f2f7b47e0
Anthony Bias <abias1122@gmail.com>
2022-07-21 04:33:31 -0400

    Handle choice for background

--- src/graphql/resolvers/backgroundResolver.js
+++ src/graphql/resolvers/backgroundResolver.js
@@ -1,6 +1,10 @@
 const Equipment = require('../../models/equipment');
 const Proficiency = require('../../models/proficiency');
 
+import Alignment from '../../models/alignment';
+import EquipmentCategory from '../../models/equipmentCategory';
+import Language from '../../models/language';
+
 const Background = {
   starting_equipment: async background => {
     const starting_equipment = background.starting_equipment;
@@ -17,6 +21,38 @@ const Background = {
     await Proficiency.find({
       index: { $in: background.starting_proficiencies.map(sp => sp.index) },
     }).lean(),
+  language_options: async background => ({
+    ...background.language_options,
+    from: {
+      option_set_type: 'options_array',
+      options: (await Language.find().lean()).map(language => ({
+        option_type: 'reference',
+        item: language,
+      })),
+    },
+  }),
+  starting_equipment_options: async background =>
+    background.starting_equipment_options.map(async option => ({
+      ...option,
+      from: {
+        ...option.from,
+        equipment_category: await EquipmentCategory.findOne({
+          index: option.from.equipment_category.index,
+        }).lean(),
+      },
+    })),
+  ideals: async background => ({
+    ...background.ideals,
+    from: {
+      ...background.ideals.from,
+      options: background.ideals.from.options.map(async option => ({
+        ...option,
+        alignments: await Alignment.find({
+          index: { $in: option.alignments.map(a => a.index) },
+        }).lean(),
+      })),
+    },
+  }),
 };
 
 module.exports = Background;
