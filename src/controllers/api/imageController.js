d19180afdfa41dd4c2cb572ac0255af757adc4be
Christopher Ward <cdurianward@gmail.com>
2022-10-03 23:49:16 -0700

    And that's the other half

--- src/controllers/api/imageController.js
+++ src/controllers/api/imageController.js
@@ -1,7 +1,24 @@
+import aws from 'aws-sdk';
+
+const s3 = new aws.S3({ params: { Bucket: 'dnd-5e-api-images' } });
+
 const show = async (req, res, next) => {
   try {
-    console.log(req);
-    return res.status(200).json({ message: 'show', imagePath: req.url });
+    const params = {
+      Bucket: 'dnd-5e-api-images',
+      Key: req.url.slice(1),
+      Range: req.headers.range,
+    };
+    return s3.getObject(params, function(err, data) {
+      if (err) {
+        res.status(200);
+        res.end('Error Fetching File');
+      } else {
+        res.attachment(params.Key); // Set Filename
+        res.type(data.ContentType); // Set FileType
+        res.send(data.Body); // Send File Buffer
+      }
+    });
   } catch (err) {
     return next(err);
   }
