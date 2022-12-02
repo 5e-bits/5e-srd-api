c06f1cb67989bfc1b837c801b712573fd8ee2b4d
Christopher Ward <cdurianward@gmail.com>
2022-12-01 19:47:05 -0800

    Convert Simple Controller

--- src/controllers/simpleController.ts
+++ src/controllers/simpleController.ts
@@ -1,16 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { Model } from 'mongoose';
+
 import { ResourceList } from '../util/data.js';
 import { escapeRegExp } from '../util/regex.js';
 
+interface IndexQuery {
+  name?: { $regex: RegExp };
+}
+
 class SimpleController {
-  constructor(Schema) {
+  Schema: Model<any>;
+
+  constructor(Schema: Model<any>) {
     this.Schema = Schema;
   }
 
-  async index(req, res, next) {
+  async index(req: Request, res: Response, next: NextFunction) {
     try {
-      const searchQueries = {};
+      const searchQueries: IndexQuery = {};
       if (req.query.name !== undefined) {
-        searchQueries.name = { $regex: new RegExp(escapeRegExp(req.query.name), 'i') };
+        searchQueries.name = { $regex: new RegExp(escapeRegExp(req.query.name as string), 'i') };
       }
 
       const data = await this.Schema.find(searchQueries)
@@ -24,7 +33,7 @@ class SimpleController {
     }
   }
 
-  async show(req, res, next) {
+  async show(req: Request, res: Response, next: NextFunction) {
     try {
       const data = await this.Schema.findOne({ index: req.params.index });
       if (!data) return next();
