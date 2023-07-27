eaff66399494bd3ed6b1fab0c0f4f6f31583ef1e
Christopher Ward <cdurianward@gmail.com>
2023-07-27 09:18:37 -0700

    Apollo 4 migration

--- src/middleware/apolloServer.ts
+++ src/middleware/apolloServer.ts
@@ -1,7 +1,7 @@
-import { ApolloServer } from 'apollo-server-express';
-import { ApolloServerPluginCacheControl } from 'apollo-server-core';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginCacheControl } from '@apollo/server/plugin/cacheControl';
 import depthLimit from 'graphql-depth-limit';
-import responseCachePlugin from 'apollo-server-plugin-response-cache';
+import responseCachePlugin from '@apollo/server-plugin-response-cache';
 import schema from '../graphql/schema.js';
 
 const createApolloMiddleware = async () => {
@@ -9,7 +9,7 @@ const createApolloMiddleware = async () => {
     schema,
     plugins: [
       // The type voodoo is needed because of a bug in apollo-server-plugin-response-cache
-      (responseCachePlugin as any).default() as any,
+      responseCachePlugin(),
       ApolloServerPluginCacheControl({
         // Cache everything for 1 second by default.
         defaultMaxAge: 3600,
