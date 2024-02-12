ac9019a8a15aaec48a05ca81891de2769e7e48e8
Christopher Ward <cdurianward@gmail.com>
2024-02-12 12:29:08 -0800

    Fix lint

--- src/js/code.js
+++ src/js/code.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 function update(call) {
   $('#interactive').val(call);
   interactive_call();
