e26b2c39730413597498003652d37acf6a9acf10
brittonhayes <me@brittonhayes.com>
2024-04-27 12:33:57 -0700

    fix: reduce error middleware response verbosity

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -4,9 +4,7 @@ const errorHandler = (err: any, req: Request, res: Response, next: NextFunction)
   console.error(err.stack);
 
   res.status(err.status || 404).json({
-    error: err,
     message: err.message,
-    details: err.errors,
   });
 };
 
