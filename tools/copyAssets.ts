c7097f7de62d3a6ec4b6d586ed5ccb2b9a4d8142
Christopher Ward <cdurianward@gmail.com>
2024-12-25 18:08:22 -0800

    fix(copyAssets): Remove copyAssets need

--- tools/copyAssets.ts
+++
@@ -1,9 +0,0 @@
-import shell from 'shelljs';
-
-shell.mkdir('-p', 'dist');
-
-// Copy all the view templates
-shell.cp('-R', 'src/css', 'dist/src');
-shell.cp('-R', 'src/public', 'dist/src');
-shell.cp('-R', 'src/views', 'dist/src');
-shell.cp('-R', 'src/swagger/api-spec', 'dist/src/swagger');
