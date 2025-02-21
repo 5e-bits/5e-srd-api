5a3f2f0e2b2768c01047bb1e047e9966d81fabbd
Fergus Bentley <fergusbentley@gmail.com>
2025-02-21 23:41:26 +0000

    Include query string in redirect url

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -5,16 +5,20 @@ export default async (req: Request, res: Response, next: NextFunction) => {
   try {
     const collections = await Collection.find({});
 
-    const colName = req.path.split('/')[1]
-    const colRequested = collections.find(col => col.index === colName)
+    const colName = req.path.split('/')[1];
+    const colRequested = collections.find(col => col.index === colName);
 
     if (colRequested === undefined && colName !== '') {
-      res.sendStatus(404)
+      res.sendStatus(404);
       return;
     }
-
-    const redirectUrl = '/api/2014' + req.path
-    res.redirect(301, redirectUrl)
+    
+    const queryString = req.originalUrl.split('?')?.[1];
+    const redirectUrl = '/api/2014' + req.path;
+    const urlWithQuery = queryString === undefined
+      ? redirectUrl
+      : redirectUrl + '?' + queryString;
+    res.redirect(301, urlWithQuery);
   } catch (err) {
     next(err);
   }
