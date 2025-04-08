d98c618b85fbac72fafb17be0773d66ded104cc2
Christopher Ward <cdurianward@gmail.com>
2025-04-08 16:20:00 -0700

    Weapon properties

--- src/models/2014/weaponProperty/index.ts
+++ src/models/2014/weaponProperty/index.ts
@@ -1,13 +1,26 @@
-import { Schema, model } from 'mongoose';
-import { WeaponProperty } from './types';
+import { getModelForClass, prop } from '@typegoose/typegoose';
+import { DocumentType } from '@typegoose/typegoose/lib/types';
 
-const WeaponPropertySchema = new Schema<WeaponProperty>({
-  _id: { type: String, select: false },
-  desc: { type: [String], index: true },
-  index: { type: String, index: true },
-  name: { type: String, index: true },
-  url: { type: String, index: true },
-  updated_at: { type: String, index: true },
+export class WeaponProperty {
+  @prop({ required: true, index: true })
+  public desc!: string[];
+
+  @prop({ required: true, index: true })
+  public index!: string;
+
+  @prop({ required: true, index: true })
+  public name!: string;
+
+  @prop({ required: true, index: true })
+  public url!: string;
+
+  @prop({ required: true, index: true })
+  public updated_at!: string;
+}
+
+export type WeaponPropertyDocument = DocumentType<WeaponProperty>;
+const WeaponPropertyModel = getModelForClass(WeaponProperty, {
+  schemaOptions: { collection: '2014-weapon-properties' },
 });
 
-export default model('WeaponProperty', WeaponPropertySchema, '2014-weapon-properties');
+export default WeaponPropertyModel;
