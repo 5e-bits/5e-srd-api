8cd7226a73cd376dd7bb27eb47c16ac7710fb364
Christopher Ward <cdurianward@gmail.com>
2025-05-04 16:20:50 -0700

    Condition

--- src/graphql/2014rewrite/resolvers/_placeholderResolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from 'type-graphql'
-
-@Resolver()
-export class PlaceholderResolver {
-  @Query(() => String) // Define a simple query returning String
-  hello(): string {
-    return 'World' // Simple implementation
-  }
-}
