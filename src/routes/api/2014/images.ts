0304291f9eaae6d309887653de6c7ec8716eed5a
Christopher Ward <cdurianward@gmail.com>
2025-05-03 18:56:02 -0700

    Move controller to correct place

--- src/routes/api/2014/images.ts
+++ src/routes/api/2014/images.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 
-import ImageController from '@/controllers/imageController'
+import ImageController from '@/controllers/api/imageController'
 
 const router = express.Router()
 
