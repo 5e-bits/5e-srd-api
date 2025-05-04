fec63b78ac075ca8c093896f6bd6c8519ac9870b
Chris Ward <cdurianward@gmail.com>
2025-05-03 19:14:08 -0700

    feat(images): Add /api/images endpoint and use fetch for image fetching (#750)
    ## What does this do?
    
    * Now that the image S3 bucket has public read access, we now fetch the
    image directly
    * Moved image controller up one level to match it's new home
    * Create `/api/images/*` route which will be the new home for images
    
    ## How was it tested?
    
    Locally
    
    ## Is there a Github issue this is resolving?
    
    Raised in the `#help` channel in Discord
    
    ## Here's a fun image for your troubles
    
    
    ![image](https://dnd-5e-api-images.s3.us-west-1.amazonaws.com/monsters/goblin.png)

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import deprecatedApiController from '@/controllers/apiController'
 import v2014Handler from './api/2014'
 import v2024Handler from './api/2024'
+import ImageHandler from './api/images'
 const router = express.Router()
 
 router.use('/2014', v2014Handler)
 router.use('/2024', v2024Handler)
+router.use('/images', ImageHandler)
 router.get('*', deprecatedApiController)
 
 export default router
