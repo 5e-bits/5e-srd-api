aca36d4b1c42ff77a8ae86558016a446567df204
Christopher Ward <cdurianward@gmail.com>
2025-05-05 23:20:27 -0700

    Subclass with trait cleanup

--- src/graphql/2014rewrite/resolvers/index.ts
+++ src/graphql/2014rewrite/resolvers/index.ts
@@ -1,30 +1,29 @@
 // This file will export an array of all resolver classes
+import { AbilityScoreResolver } from './abilityScoreResolver'
 import { AlignmentResolver } from './alignmentResolver'
+import { BackgroundResolver, EquipmentRefResolver } from './backgroundResolver'
+import { ClassResolver, MultiClassingResolver, MultiClassingPrereqResolver } from './classResolver'
 import { ConditionResolver } from './conditionResolver'
 import { DamageTypeResolver } from './damageTypeResolver'
-import { LanguageResolver } from './languageResolver'
-import { MagicSchoolResolver } from './magicSchoolResolver'
-import { RuleSectionResolver } from './ruleSectionResolver'
-import { WeaponPropertyResolver } from './weaponPropertyResolver'
 import { EquipmentCategoryResolver } from './equipmentCategoryResolver'
-import { AbilityScoreResolver } from './abilityScoreResolver'
-import { SkillResolver } from './skillResolver'
-import { BackgroundResolver, EquipmentRefResolver } from './backgroundResolver'
-import { FeatResolver, PrerequisiteResolver } from './featResolver'
-import { RuleResolver } from './ruleResolver'
-import { TraitResolver, TraitSpecificResolver } from './traitResolver'
-import { MagicItemResolver } from './magicItemResolver'
-import { SubraceResolver, SubraceAbilityBonusResolver } from './subraceResolver'
 import { EquipmentResolver } from './equipmentResolver'
-import { ProficiencyResolver } from './proficiencyResolver'
+import { FeatResolver, PrerequisiteResolver } from './featResolver'
 import { FeatureResolver, FeatureSpecificResolver } from './featureResolver'
-import { RaceResolver, RaceAbilityBonusResolver } from './raceResolver'
-import { SpellResolver } from './spellResolver'
+import { LanguageResolver } from './languageResolver'
 import { LevelResolver } from './levelResolver'
-import { SubclassResolver } from './subclassResolver'
-import { ClassResolver, MultiClassingResolver, MultiClassingPrereqResolver } from './classResolver'
+import { MagicItemResolver } from './magicItemResolver'
+import { MagicSchoolResolver } from './magicSchoolResolver'
 import { MonsterResolver } from './monsterResolver'
-import { ActionDamageResolver } from './actionDamageResolver'
+import { ProficiencyResolver } from './proficiencyResolver'
+import { RaceResolver, RaceAbilityBonusResolver } from './raceResolver'
+import { RuleResolver } from './ruleResolver'
+import { RuleSectionResolver } from './ruleSectionResolver'
+import { SkillResolver } from './skillResolver'
+import { SpellResolver } from './spellResolver'
+import { SubclassResolver, SubclassSpellResolver } from './subclassResolver'
+import { SubraceResolver, SubraceAbilityBonusResolver } from './subraceResolver'
+import { TraitResolver, TraitSpecificResolver, ActionDamageResolver } from './traitResolver'
+import { WeaponPropertyResolver } from './weaponPropertyResolver'
 
 const collectionResolvers = [
   AbilityScoreResolver,
@@ -70,8 +69,10 @@ const fieldResolvers = [
   // Class
   MultiClassingResolver,
   MultiClassingPrereqResolver,
-  // ActionDamage
-  ActionDamageResolver
+  // Trait
+  ActionDamageResolver,
+  // Subclass
+  SubclassSpellResolver
 ] as const
 
 // Export a new mutable array combining the readonly ones
