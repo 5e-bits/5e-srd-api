1227b4bf74b143c913bd0571b56b7a966dd1431c
Christopher Ward <cdurianward@gmail.com>
2025-05-21 20:10:49 -0700

    Stab the last circular depedency

--- src/graphql/2014/common/unions.ts
+++ src/graphql/2014/common/unions.ts
@@ -1,8 +1,27 @@
-import { createUnionType } from 'type-graphql'
+import { createUnionType, ClassType } from 'type-graphql'
 import { MagicItem } from '@/models/2014/magicItem'
-import { Armor, Weapon, Tool, Gear, Pack, Ammunition, Vehicle } from './equipmentTypes'
+import type { Armor, Weapon, Tool, Gear, Pack, Ammunition, Vehicle } from './equipmentTypes'
 import { Proficiency } from '@/models/2014/proficiency'
 import { ProficiencyChoice } from '@/graphql/2014/common/choiceTypes'
+import { customRequire } from '../utils/helpers'
+
+interface EquipmentTypesModule {
+  Armor: ClassType<Armor>
+  Weapon: ClassType<Weapon>
+  Tool: ClassType<Tool>
+  Gear: ClassType<Gear>
+  Pack: ClassType<Pack>
+  Ammunition: ClassType<Ammunition>
+  Vehicle: ClassType<Vehicle>
+}
+
+let equipmentTypesModule: EquipmentTypesModule | null = null
+const getEquipmentTypes = (): EquipmentTypesModule => {
+  if (!equipmentTypesModule) {
+    equipmentTypesModule = customRequire('../common/equipmentTypes') as EquipmentTypesModule
+  }
+  return equipmentTypesModule
+}
 
 function resolveEquipmentType(
   value: any
@@ -15,6 +34,7 @@ function resolveEquipmentType(
   | typeof Ammunition
   | typeof Vehicle
   | null {
+  const { Armor, Weapon, Tool, Gear, Pack, Ammunition, Vehicle } = getEquipmentTypes()
   if ('armor_class' in value) {
     return Armor
   }
@@ -41,7 +61,19 @@ function resolveEquipmentType(
 
 export const EquipmentOrMagicItem = createUnionType({
   name: 'EquipmentOrMagicItem',
-  types: () => [Armor, Weapon, Tool, Gear, Pack, Ammunition, Vehicle, MagicItem] as const,
+  types: () => {
+    const et = getEquipmentTypes()
+    return [
+      et.Armor,
+      et.Weapon,
+      et.Tool,
+      et.Gear,
+      et.Pack,
+      et.Ammunition,
+      et.Vehicle,
+      MagicItem
+    ] as const
+  },
   resolveType: (value) => {
     if ('rarity' in value) {
       return MagicItem
@@ -59,7 +91,10 @@ export const EquipmentOrMagicItem = createUnionType({
 
 export const AnyEquipment = createUnionType({
   name: 'AnyEquipment',
-  types: () => [Armor, Weapon, Tool, Gear, Pack, Ammunition, Vehicle] as const,
+  types: () => {
+    const et = getEquipmentTypes()
+    return [et.Armor, et.Weapon, et.Tool, et.Gear, et.Pack, et.Ammunition, et.Vehicle] as const
+  },
   resolveType: (value) => {
     const equipmentType = resolveEquipmentType(value)
     if (equipmentType) {
@@ -67,6 +102,7 @@ export const AnyEquipment = createUnionType({
     }
 
     console.warn('Could not resolve type for AnyEquipment:', value)
+    const { Gear } = getEquipmentTypes()
     return Gear
   }
 })
