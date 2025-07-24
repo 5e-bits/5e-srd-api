6590413d311a64b812c1b1495b4974c5931ed3df
Christopher Ward <cdurianward@gmail.com>
2025-07-24 14:03:17 -0700

    Refactor redis client

--- src/util/RedisClient.ts
+++ src/util/RedisClient.ts
@@ -2,10 +2,16 @@ import { createClient } from 'redis'
 
 import { redisUrl } from './environmentVariables'
 
+const isTls = redisUrl.match(/rediss:/) != null
+const parsedUrl = new URL(redisUrl)
+
 export default createClient({
   url: redisUrl,
-  socket: {
-    tls: redisUrl.match(/rediss:/) != null,
-    rejectUnauthorized: false
-  }
+  socket: isTls
+    ? {
+        tls: true,
+        host: parsedUrl.hostname,
+        rejectUnauthorized: false
+      }
+    : undefined
 })
